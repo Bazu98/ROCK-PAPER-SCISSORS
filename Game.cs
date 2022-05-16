b28b4d0ff91a2d944b1efbffa1d7df975e1ad660
Bazu98 <v.bazuashvili@gmail.com>
2022-05-16 09:47:16 -0700

    Added new project

---
+++ Game.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Math;
+
+namespace ROCKPAPERSCISSORS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPlayer, inputCPU;
+            int randomInt;
+            int scorePlayer = 0;
+            int scoreCPU = 0;
+            bool playAgain = true;
+
+            while(playAgain)
+            {
+
+            
+
+
+            while(scorePlayer < 3 && scoreCPU < 3)
+            {
+
+            
+
+            Console.Write("Choose between ROCK, PAPER, and SCISSORS:        ");
+            inputPlayer = Console.ReadLine();
+            inputPlayer = inputPlayer.ToUpper();
+
+            Random rnd = new Random;
+
+            randomInt = rnd.Next(1,4);
+
+            switch (randomInt)
+            {
+                case 1:
+                inputCPU = "ROCK";
+                Console.WriteLine("Computer chose ROCK");
+                if(inputPlayer == "ROCK")
+                {
+                    Console.WriteLine("DRAW ! \n\n");
+                }
+                else if(inputPlayer == "PAPER") 
+                {
+                    Console.WriteLine("PLAYER WINS ! \n\n");
+                    scorePlayer ++;
+                }
+                else if(inputPlayer == "SCISSORS")
+                {
+                    Console.WriteLine("CPU WINS ! \n\n");
+                    scoreCPU ++;
+                }
+                
+                break;
+
+                case 2:
+                inputCPU = "PAPER";
+                Console.WriteLine("Computer chose PAPER");
+                if(inputPlayer == "ROCK")
+                {
+                    Console.WriteLine("DRAW ! \n\n");
+                }
+                else if(inputPlayer == "ROCK") 
+                {
+                    Console.WriteLine("PLAYER WINS ! \n\n");
+                    scorePlayer ++;
+                }
+                else if(inputPlayer == "SCISSORS")
+                {
+                    Console.WriteLine("CPU WINS ! \n\n");
+                    scoreCPU ++;
+                }
+                break;
+
+                case 3:
+                inputCPU = "SCISSORS";
+                Console.WriteLine("Computer chose SCISSORS");
+                if(inputPlayer == "ROCK")
+                {
+                    Console.WriteLine("DRAW ! \n\n");
+                }
+                else if(inputPlayer == "ROCK") 
+                {
+                    Console.WriteLine("PLAYER WINS ! \n\n");
+                    scorePlayer ++;
+                }
+                else if(inputPlayer == "PAPER")
+                {
+                    Console.WriteLine("CPU WINS ! \n\n");
+                    scoreCPU ++;
+                }
+                break;
+                default:
+                Console.WriteLine("Invalid entry");
+                break;
+            }
+
+            Console.WriteLine("\n\nSCORES:\tPLAYER:\t{0}\tCPU:\t{1}", scorePlayer,scoreCPU);
+
+            }
+            if(scorePlayer == 3)
+            {
+                Console.WriteLine("Player WON !");
+            }
+            else if(scoreCPU == 3)
+            {
+                Console.WriteLine("CPU WON !");
+            }
+            else
+            {
+
+            }
+            Console.WriteLine("Do you want to play agani ?(y\n) ");
+
+            string loop = Console.ReadLine();
+            if(loop == "y")
+            {
+                playAgain = true;
+                Console.Clear();
+            }
+            else if(loop == "n") 
+            {
+                playAgain = false;
+            }
+            else
+            {
+            
+            }
+        }
+    }
+
+    }
+}
